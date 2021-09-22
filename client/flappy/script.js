f10bcd572df9d03e406b66e5781d95bde395d19c
Jayid Alam <jayid_07@yahoo.com>
2021-09-22 19:26:35 -0400

    added image of bird and added left, right and top border

--- client/flappy/script.js
+++ client/flappy/script.js
@@ -3,10 +3,13 @@ document.addEventListener('DOMContentLoaded' , () => {
         gameDisplay = document.querySelector('.game-container'),
         ground = document.querySelector('.ground');
 
+
+    //starting position of the bird
     let birdLeft = 220,
         birdBottom = 100,
         gravity = 2,
-        isGameOver = false;
+        isGameOver = false,
+        spaceBetWeen = 440;
 
     function startGame() {
         birdBottom -= gravity;
@@ -23,7 +26,7 @@ document.addEventListener('DOMContentLoaded' , () => {
     }
 
     function jump() { //makes the bird jump
-        if (birdBottom < 500) birdBottom += 50;
+        if (birdBottom < 500 && !isGameOver) birdBottom += 50;
        
         bird.style.bottom = birdBottom + 'px';
         console.log(birdBottom)
@@ -31,28 +34,39 @@ document.addEventListener('DOMContentLoaded' , () => {
 
     document.addEventListener('keyup', jump)
 
-    function generateObstacle () {
+    function generateObstacle () { //generate obstacles or pipes
         let obstacleLeft = 500,
             randomHeight = Math.random() * 60,
             obstacleBottom = randomHeight;
-        const obstacle = document.createElement('div');
+        const obstacle = document.createElement('div'),
+            topObstacle = document.createElement('div');
 
-        if (!isGameOver) obstacle.classList.add('obstacle');
+        if (!isGameOver) {
+            obstacle.classList.add('obstacle');
+            topObstacle.classList.add('topObstacle');
+        }
+
         gameDisplay.appendChild(obstacle);
+        gameDisplay.appendChild(topObstacle);
         obstacle.style.left = obstacleLeft + 'px';
+        topObstacle.style.left = obstacleLeft + 'px';
         obstacle.style.bottom = obstacleBottom + 'px';
+        topObstacle.style.bottom = obstacleBottom + spaceBetWeen + 'px';
 
         function moveObstacle() {
             obstacleLeft -= 2;
             obstacle.style.left = obstacleLeft + 'px';
+            topObstacle.style.left = obstacleLeft + 'px';
 
             if (obstacleLeft === -60) {
                 clearInterval(timerID);
                 gameDisplay.removeChild(obstacle)
+                gameDisplay.removeChild(topObstacle)
             }
 
-            if (obstacleLeft > 200 && obstacleLeft < 280 && 
-                birdLeft === 220 && birdBottom < (obstacleBottom + 150) || birdBottom === 0) {
+            if (obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 && 
+                (birdBottom < (obstacleBottom + 150) || birdBottom > obstacleBottom + spaceBetWeen - 200) || 
+                birdBottom === 0) {
                 gameOver();
                 clearInterval(timerID);
             }
