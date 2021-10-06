67333ce4f4838cfcab56df0c32cc2035d8006f83
Jayid Alam <jayid_07@yahoo.com>
2021-10-06 12:57:19 -0400

    Added the styles.css file

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,3 +1,8 @@
-document.addEventListener('DOMContentLoaded', () {
-    
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid'))
+    const width = 10  
+
+    console.log(squares)
+
 })
