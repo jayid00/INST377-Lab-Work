c458c41f5c01cdfc3e478cfc881603fefad2a2f5
Jayid Alam <jayid_07@yahoo.com>
2021-10-06 13:01:04 -0400

    forgot to add the styles.css

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     let squares = Array.from(document.querySelectorAll('.grid'))
+    const scoreDisplay = document.querySelector('#')
     const width = 10  
 
-    console.log(squares)
-
+    
 })
