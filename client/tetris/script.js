828500de8f11095c7644bc6483b53a823499b7ad
Jayid Alam <jayid_07@yahoo.com>
2021-10-06 22:02:25 -0400

    implemented moveRight and left func

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -64,5 +64,76 @@ document.addEventListener('DOMContentLoaded', () => {
           squares[currentPosition + index].classList.remove('tetromino')
       })
   }
+
+  //make the tetromino move down every second
+  timerID = setInterval(moveDown, 250)
+
+  //Assign functions to KeyCodes
+  function control(e) {
+    if (e.keyCode === 37) { //right arrow
+      moveLeft()
+    }
+    else if (e.keyCode === 38) { //up arrow
+      //rotate
+    }
+    else if (e.keyCode === 39) {
+      moveRight()
+    }
+    else if (e.keyCode === 40) {
+      moveDown()
+    }
+    
+  }
+  document.addEventListener('keyup', control)
+
+  function moveDown() {
+    undraw()
+    currentPosition += width
+    draw()
+    freeze()
+  }
+
+
+  //freeze function
+  function freeze() {
+    if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+      current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+      random = Math.floor(Math.random() * theTetrominos.length)
+      current = theTetrominos[random][curerntRotation]
+      currentPosition = 4
+      draw()
+    }
+  }
+
+  //limit the tetromin moving out of bound on the left
+
+  function moveLeft() {
+    undraw()
+   
+    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+   
+    if (!isAtLeftEdge) currentPosition -=1
+
+    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition +=1 
+    }
+
+    draw()
+  }
+
+
+  function moveRight() {
+    undraw()
+   
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1)
+    
+    if (!isAtRightEdge) currentPosition +=1
+
+    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition -=1 
+    }
+
+    draw()
+  }
   
 })
