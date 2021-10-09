792e7aa7193814cedf1c6f01c295efac57fc831d
Jayid Alam <jayid_07@yahoo.com>
2021-10-09 11:29:33 -0400

    wrapped the code around async function

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,39 +1,43 @@
-const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'
-//const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
+async function windowFunctions() {
+  const endpoint ="https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
+  
+  const request = await fetch(endpoint)
 
-const restaurant = [];
+  const restaurant = await request.json();
 
- fetch(endpoint)
-    .then(blob => blob.json())
-    .then(data => restaurant.push(...data))
+//   fetch(endpoint)
+//     .then((blob) => blob.json())
+//     .then((data) => restaurant.push(...data));
 
-function findMatches(wordToMatch, restaurant) {
-    return restaurant.filter(place => {
-
-        const regex = new RegExp(wordToMatch, 'gi');
-        return place.zip.match(regex) || place.category.match(regex)
+  function findMatches(wordToMatch, restaurant) {
+    return restaurant.filter((place) => {
+      const regex = new RegExp(wordToMatch, "gi");
+      return place.zip.match(regex) || place.category.match(regex);
     });
+  }
 
-}
-
-
-function displayMatches() {
-    const matchArray = findMatches(this.value, restaurant)
-    const html = matchArray.map(place => {
+  function displayMatches(e) {
+    const matchArray = findMatches(e.target.value, restaurant);
+    const html = matchArray
+      .map((place) => {
         return `
-        <li>
-            <span>${place.name}</span><br>
-            <span>${place.category}</span><br>
-            <span>${place.address_line_1}</span><br>
-            <span>${place.city}</span>
-            <span>${place.zip}</span>
-          </li>  `
-    }).join('');
+        <li><div>${place.name}</div></li>
+            <div>${place.category}</div>
+            <div>${place.address_line_1}</div>
+            <div>${place.city}</div>
+            <div>${place.zip}</div>
+          <br>  `;
+      })
+      .join("");
 
     suggestions.innerHTML = html;
+  }
+
+  const searchInput = document.querySelector(".input");
+  const suggestions = document.querySelector(".suggestions");
+
+  searchInput.addEventListener("keyup", (evt) => { displayMatches(evt) });
 }
 
-const searchInput = document.querySelector('.input')
-const suggestions = document.querySelector('.suggestions')
 
-searchInput.addEventListener('keyup', displayMatches)
+window.onload = windowFunctions;
