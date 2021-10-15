6778f2f3e70e68d02f23a015638dbab20d6f2784
Jayid Alam <jayid_07@yahoo.com>
2021-10-14 22:19:18 -0400

    Got the map working

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -1,10 +1,21 @@
+
 async function windowFunctions() {
     const endpoint ="https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
-    
     const request = await fetch(endpoint)
-  
     const restaurant = await request.json();
+    const mymap = L.map('mapid').setView([51.505, -0.09], 12);
+    const token = 'pk.eyJ1IjoiamF5aWQwNyIsImEiOiJja3VycWtzbDg1OHoyMzJuenl3YTBrZ3piIn0._AtDIQVdV3BnDGihF3xusw'
   
+
+    L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${token}`, {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
+
     function findMatches(wordToMatch, restaurant) {
       return restaurant.filter((place) => {
         const regex = new RegExp(wordToMatch, "gi");
