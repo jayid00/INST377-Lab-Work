512f920186d8a09119af1eded4de6d064a64ab49
Jayid Alam <jayid_07@yahoo.com>
2021-10-24 16:29:48 -0400

    Finally was able to add markers to the map for matched restaurants

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -3,11 +3,11 @@ async function windowFunctions() {
     const endpoint ="https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
     const request = await fetch(endpoint)
     const restaurant = await request.json();
-    const mymap = L.map('mapid').setView([51.505, -0.09], 12);
+    const mymap = L.map('mapid').setView([38.989, -76.938], 12);
     const searchInput = document.querySelector(".input");
     const suggestions = document.querySelector(".suggestions");
-    const token = 'pk.eyJ1IjoiamF5aWQwNyIsImEiOiJja3VycWtzbDg1OHoyMzJuenl3YTBrZ3piIn0._AtDIQVdV3BnDGihF3xusw'
-  
+    const token = 'pk.eyJ1IjoiamF5aWQwNyIsImEiOiJja3VycWtzbDg1OHoyMzJuenl3YTBrZ3piIn0._AtDIQVdV3BnDGihF3xusw';
+    let matchedRest = [];
 
     L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${token}`, {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -18,7 +18,7 @@ async function windowFunctions() {
     accessToken: 'your.mapbox.access.token'
 }).addTo(mymap);
 
-
+// Finds the matched zipcode
     function findMatches(wordToMatch, restaurant) {
       return restaurant.filter((place) => {
         const regex = new RegExp(wordToMatch, "gi");
@@ -26,6 +26,7 @@ async function windowFunctions() {
       });
     }
   
+  // Displays the matched zipcodes as a list
     function displayMatches(e) {
       let matchArray = findMatches(e.target.value, restaurant);
       matchArray = matchArray.slice(0, 5);
@@ -37,8 +38,7 @@ async function windowFunctions() {
             <br>  `;
         })
         .join("");
-  
-  
+
         // clear the result when no input provided
         if (!e.target.value) {
           document.querySelector(".suggestions").innerHTML = "";
@@ -47,11 +47,34 @@ async function windowFunctions() {
   
         //show the suggested result
       suggestions.innerHTML = html;
+      matchedRest = matchArray;
     }
   
    
+    function map(matchArray) {
+
+      let count = 0;
+      mymap.eachLayer(Layer => {
+        if (Layer._latlng !== undefined) {
+          Layer.remove();
+        }
+      });
+
+
+      matchArray.forEach(element => {
+        const long = element.geocoded_column_1.coordinates[0];
+        const lat = element.geocoded_column_1.coordinates[1];
+        
+        if (count === 0) {
+          mymap.panTo([lat, long]);
+        }
+        count++;
+        L.marker([lat, long]).addTo(mymap);
+      });
+    }
   
-    searchInput.addEventListener("keyup", (evt) => { displayMatches(evt) });
+
+    searchInput.addEventListener("keyup", (evt) => { displayMatches(evt), map(matchedRest) });
   }
   
   
